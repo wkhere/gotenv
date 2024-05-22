e91e4b6d8e03ae6d467d685553f88a27370e10df
Wojciech Kaczmarek <wojtekk@kofeina.net>
2024-05-22 14:31:32 +0200

    api doc

--- load.go
+++ load.go
@@ -9,6 +9,21 @@ type envvar struct {
 	key, val string
 }
 
+// Load given dotenv files. As a result, the env variables within
+// the current process are set according to the assignments files;
+// they should have a form `NAME=VALUE`.
+// Whitespaces around equal sign `=`
+// are allowed and ignored.
+// If the value is surrounded by the double quotes `"`,
+// it will be passed to the env unquoted.
+// No value after equal sign means that the variable will be reset to empty.
+// Hash sign `#` acts as a comment, ignoring everything until the end of line,
+// unless it's within double quotes.
+//
+// Important: if the variable existed in the env prior to this call,
+// it will *not* be set.
+//
+// If there is no given file, `.env` is assumed.
 func Load(filenames ...string) (err error) {
 	if len(filenames) == 0 {
 		filenames = []string{".env"}
